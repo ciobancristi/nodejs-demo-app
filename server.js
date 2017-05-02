db40252867b524d6d4772f74dc281b0ec19a2ccf
unknown <ciobancristi@gmail.com>
2017-05-03 00:11:09 +0300

    modified template v1

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // grab the packages we need
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 8080;
+
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/src'));
 
