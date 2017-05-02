6c1e4d019d1c54c16b4b75bd54242b9121c797de
unknown <ciobancristi@gmail.com>
2017-05-02 22:37:08 +0300

    added peak-web template

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 
+app.use(express.static(__dirname + '/src'));
+
 // routes will go here
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  response.render('src/index.html');
 })
 
 // start the server
-app.listen(port);
-console.log('Server started! At http://localhost:' + port);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
